ad05beb9dcb2cb1b78a9fc2b3d0c07ff6da8aa10
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-09-10 18:23:40 -0400

    add images and onMouseMove function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+
+  MouseMove = (e)=> {
+        var wrapper = document.getElementById('wrapper');
+        var topLayer = wrapper.querySelector('.top');
+        var handle = wrapper.querySelector('.handle');
+        var skew = 0;
+        var delta = 0;
+
+         if(wrapper.className.indexOf('skewed') != -1){
+           skew = 1000;
+          }
+        delta = (e.clientX - window.innerWidth / 2) * 0.5;
+      
+        handle.style.left = e.clientX + delta + 'px';
+
+        topLayer.style.width= e.clientX + skew + delta + 'px';
+  }
+
+
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <section id="wrapper" className="skewed" onMouseMove={this.MouseMove}>
+
+          <div className="layer bottom">
+            <div className="content-wrap">
+              <div className="content-body">
+                <h1>Look Sharp</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quisquam temporibus dolore vero reiciendis atque debitis. Sequi at consequatur deserunt?</p>
+              </div>
+              <img src={require("./img/home.jpg")} alt=""/>
+            </div>
+          </div>
+
+          <div className="layer top">
+              <div className="content-wrap">
+                <div className="content-body">
+                  <h1>Stay Cool</h1>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quisquam temporibus dolore vero reiciendis atque debitis. Sequi at consequatur deserunt?</p>
+                </div>
+                <img src={require("./img/homebanner.png")} alt=""/>
+              </div>
+            </div>
+
+       <div className="handle"></div>
+
+  </section>
     );
   }
 }
